afdc58830a46422b14a2f2d7f42b394e0ca5b827
Gen <archiherewego@gmail.com>
2018-03-11 18:17:00 +0900

    Add link to ListActivity

--- app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/MainActivity.kt
@@ -18,21 +18,24 @@ package jp.gcreate.sample.samplearchitecturecomponent
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import jp.gcreate.sample.samplearchitecturecomponent.databinding.ActivityMainBinding
+import jp.gcreate.sample.samplearchitecturecomponent.list.ListActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by lazy { ViewModelProviders.of(this).get(MainViewModel::class.java) }
-    private val textOne: TextView by lazy { findViewById<TextView>(R.id.text_one) }
-    private val textTwo: TextView by lazy { findViewById<TextView>(R.id.text_two) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        textOne.text = this.toString()
-        viewModel.viewModelInstance.observe(this, Observer { textTwo.text = it ?: "instance is null" })
+        binding.textOne.text = this.toString()
+        viewModel.viewModelInstance.observe(this, Observer { binding.textTwo.text = it ?: "instance is null" })
+        binding.openList.setOnClickListener { startActivity(Intent(this, ListActivity::class.java)) }
     }
 }

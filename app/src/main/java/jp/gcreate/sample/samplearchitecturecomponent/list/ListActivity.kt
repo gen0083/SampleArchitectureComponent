be03a169e6534464c9e55e5d8f348ae8271f4b1a
Gen <archiherewego@gmail.com>
2018-03-11 18:05:56 +0900

    Add activity to using RecyclerView

---
+++ app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/list/ListActivity.kt
@@ -0,0 +1,16 @@
+package jp.gcreate.sample.samplearchitecturecomponent.list
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import jp.gcreate.sample.samplearchitecturecomponent.R
+
+/**
+ * Copyright 2018 G-CREATE
+ */
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}

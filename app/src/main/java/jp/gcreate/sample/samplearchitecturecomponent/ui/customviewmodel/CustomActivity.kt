25075f85c72a7e94f4f8a4113ebdf12d2be52895
Gen <archiherewego@gmail.com>
2018-03-15 00:25:33 +0900

    Show error message with LiveData

--- app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/ui/customviewmodel/CustomActivity.kt
+++ app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/ui/customviewmodel/CustomActivity.kt
@@ -5,12 +5,9 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.text.Editable
-import android.text.TextWatcher
 import jp.gcreate.sample.samplearchitecturecomponent.App
 import jp.gcreate.sample.samplearchitecturecomponent.R
 import jp.gcreate.sample.samplearchitecturecomponent.databinding.ActivityCustomBinding
@@ -31,32 +28,6 @@ class CustomActivity : AppCompatActivity() {
 
         setUpRecyclerView()
 
-        binding.inputName.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable) {
-                setErrorMessage(binding.inputName)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                setErrorMessage(binding.inputName)
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                setErrorMessage(binding.inputName)
-            }
-        })
-        binding.inputValue.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                setErrorMessage(binding.inputValue)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                setErrorMessage(binding.inputValue)
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                setErrorMessage(binding.inputValue)
-            }
-        })
         binding.addButton.setOnClickListener {
             val success = viewModel.addData(binding.inputName.editableText.toString(),
                               binding.inputValue.editableText.toString())
@@ -71,6 +42,12 @@ class CustomActivity : AppCompatActivity() {
         viewModel.testData.observe(this, Observer {
             listAdapter.submitList(it)
         })
+        viewModel.nameError.observe(this, Observer {
+            binding.inputName.error = it
+        })
+        viewModel.valueError.observe(this, Observer {
+            binding.inputValue.error = it
+        })
     }
 
     private fun setUpRecyclerView() {
@@ -78,14 +55,6 @@ class CustomActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@CustomActivity, LinearLayoutManager.VERTICAL, true)
             adapter = listAdapter
             addItemDecoration(DividerItemDecoration(this@CustomActivity, LinearLayoutManager.VERTICAL))
-        }
-    }
-
-    private fun setErrorMessage(editText: TextInputEditText) {
-        if (editText.editableText.toString().isEmpty()) {
-            editText.error = "empty is not allowed"
-        } else {
-            editText.error = null
         }
     }
 }
--- app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/ui/customviewmodel/CustomViewModel.kt
+++ app/src/main/java/jp/gcreate/sample/samplearchitecturecomponent/ui/customviewmodel/CustomViewModel.kt
@@ -2,6 +2,7 @@ package jp.gcreate.sample.samplearchitecturecomponent.ui.customviewmodel
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
 import jp.gcreate.sample.samplearchitecturecomponent.data.TestData
 import jp.gcreate.sample.samplearchitecturecomponent.data.repository.TestRepository
@@ -10,15 +11,29 @@ import timber.log.Timber
 /**
  * Copyright 2018 G-CREATE
  */
+
+private const val EMPTY_MESSAGE = "empty not allowed"
+
 class CustomViewModel(private val repository: TestRepository): ViewModel() {
     private val dispatcherTestData: MutableLiveData<List<TestData>> = MutableLiveData()
     val testData: LiveData<List<TestData>> = dispatcherTestData
+    private val dispatcherNameError: MutableLiveData<String?> = MutableLiveData()
+    val nameError: LiveData<String?> = dispatcherNameError
+    private val dispatcherValueError: MutableLiveData<String?> = MutableLiveData()
+    val valueError: LiveData<String?> = dispatcherValueError
+
+    private val testObserver = Observer<List<TestData>> {
+        Timber.v("observer value changed: item count=${it?.size}")
+    }
 
     init {
         dispatcherTestData.value = repository.loadAll()
+        dispatcherTestData.observeForever(testObserver)
     }
 
     fun addData(name: String, value: String): Boolean {
+        dispatcherNameError.value = if (name.isEmpty()) EMPTY_MESSAGE else null
+        dispatcherValueError.value = if (value.isEmpty()) EMPTY_MESSAGE else null
         if (name.isEmpty() || value.isEmpty()) return false
 
         val data = TestData(name, value)
@@ -30,5 +45,6 @@ class CustomViewModel(private val repository: TestRepository): ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Timber.v("onCleared")
+        dispatcherTestData.removeObserver(testObserver)
     }
 }

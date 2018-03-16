2a1eb12c7d0f7712f1ff94534eb66820de71630c
Gen <archiherewego@gmail.com>
2018-03-16 21:52:08 +0900

    Update test with LiveData

--- app/src/androidTest/java/jp/gcreate/sample/samplearchitecturecomponent/data/repository/TestRepositoryByRoomTest.kt
+++ app/src/androidTest/java/jp/gcreate/sample/samplearchitecturecomponent/data/repository/TestRepositoryByRoomTest.kt
@@ -1,12 +1,19 @@
 package jp.gcreate.sample.samplearchitecturecomponent.data.repository
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
 import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import jp.gcreate.sample.samplearchitecturecomponent.data.TestData
 import jp.gcreate.sample.samplearchitecturecomponent.data.room.AppDatabase
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,10 +24,12 @@ import org.junit.runner.RunWith
 class TestRepositoryByRoomTest {
     private lateinit var sut: TestRepositoryByRoom
     private lateinit var db: AppDatabase
+    @get:Rule var executorRule = InstantTaskExecutorRule()
 
     @Before fun setUp() {
         val context = InstrumentationRegistry.getTargetContext()
-        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .allowMainThreadQueries().build()
         sut = TestRepositoryByRoom(db.testDataDao())
     }
 
@@ -33,5 +42,28 @@ class TestRepositoryByRoomTest {
         sut.save(data)
 
         val actual = sut.load("test")
+        assertThat(actual, `is`(notNullValue()))
+        // id is not same, caused by auto increment
+        assertThat(actual!!.name, `is`(data.name))
+    }
+
+    @Test fun insertDataTwice() {
+        var list = listOf<TestData>()
+        val observer = Observer<List<TestData>> {
+            it?.let { list = it }
+        }
+
+        sut.monitorAll().observeForever(observer)
+
+        sut.save(TestData(0, "test1", "test1 data"))
+        sut.save(TestData(0, "test2", "test2 data"))
+
+        assertThat(sut.load("test1"), `is`(TestData(1, "test1", "test1 data")))
+        assertThat(sut.load("test2"), `is`(TestData(2, "test2", "test2 data")))
+
+        assertThat(list.size, `is`(2))
+        assertThat(sut.monitorAll().value, `is`(nullValue()))
+
+        sut.monitorAll().removeObserver(observer)
     }
 }
